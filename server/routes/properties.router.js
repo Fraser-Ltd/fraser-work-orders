831f18870a2595cd585a59ef5e38ea83357a3a16
Jeffrey L McMahon <jeffmcmahon1973@gmail.com>
2020-11-20 14:50:59 -0600

    final bug fixes

--- server/routes/properties.router.js
+++ server/routes/properties.router.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated };
 
 // GET Route /api/properties/
+// Will deliver the entire property list
 
-router.get('/', (req, res) => {
-  // GET route code here
+router.get("/", rejectUnauthenticated, (req, res) => {
+
+  const queryText = `SELECT * FROM "properties"`;
+  pool
+    .query(queryText)
+    .then((result) => res.send(result.rows))
+    .catch((err) => {
+      console.log('Error in properties GET request', err);
+      res.sendStatus(500);
+    })
 });
-
-
 
 
 //  (POST)  /api/properties/
@@ -18,13 +26,22 @@ router.get('/', (req, res) => {
 //        propertyAddress: value,
 //        residentCoordinator: value,
 //       }
-router.post('/', (req, res) => {
-  // POST route code here
+
+router.post("/", rejectUnauthenticated, (req, res) => {
+
+  const propertyName = req.body.propertyName
+  const propertyAddress = req.body.propertyAddress
+  const residentCoordinator = req.body.residentCoordinator
+  const queryText = `INSERT INTO "properties" ("property_name", "property_address", "resident_coordinator")
+                      VALUES ($1, $2, #3)`;
+  pool
+    .query(queryText, [propertyName, propertyAddress, residentCoordinator])
+    .then((result) => res.sendStatus(200))
+    .catch((err) => {
+      console.log('Error in property POST route', err);
+      res.sendStatus(500);
+    })
 });
-
-
-
-
 
 //  (PUT)       /api/properties/:id
 //  Incomming body should look like this:
@@ -34,9 +51,36 @@ router.post('/', (req, res) => {
 //        residentCoordinator: value,
 //       }
 
+router.put('/:id', rejectUnauthenticated, (req, res) => {
 
+  const propId = req.body.id
+  const propName = req.body.propertyName
+  const propAdd = req.body.propertyAddress
+  const propRes = req.body.residentCoordinator
+
+  let queryText = `UPDATE "properties" SET "property_name" = $1, "property_address" = $2, "resident_coordinator" = $3)
+                 WHERE "id" =$4`
+  pool.query(queryText, [propName, propAdd, propRes, propId])
+    .then(result => {
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      console.log('Error in property PUT route', err);
+      res.sendStatus(500);
+    })
+})
 
 //  (DELETE)    /api/properties/:id
 
+router.delete('/:id', (req, res) => {
+  let queryText = `DELETE FROM "properties" WHERE id=$1`;
+  pool.query(queryText, [req.params.id])
+    .then(result => {
+      res.send('Succesfully Deleted Property');
+    }).catch((err) => {
+      console.log('Error in property DELETE route', err);
+      res.sendStatus(500);
+    })
+})
 
 module.exports = router;
