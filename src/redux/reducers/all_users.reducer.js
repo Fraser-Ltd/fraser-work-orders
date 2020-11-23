9ee569c50574f1081237798ef707507723661455
Sarah Carlisle <sarah@travelescaperoom.com>
2020-11-23 16:10:11 -0600

    fixed router

--- src/redux/reducers/all_users.reducer.js
+++ src/redux/reducers/all_users.reducer.js
@@ -1,23 +1,9 @@
 const allUsers = (state = {}, action) => {
     switch (action.type) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> bd870c67ed825f5247c42c963999538ce6065769
       case 'SET_USERS':
             return action.payload;
         default:
             return state;
-<<<<<<< HEAD
-=======
-
-      default:
-        return state
-
->>>>>>> 615ec2c738010488d69463296da31daf3abd7688
-=======
-
->>>>>>> bd870c67ed825f5247c42c963999538ce6065769
     }
   };
 
