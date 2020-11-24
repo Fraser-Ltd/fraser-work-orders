45d094f36dd3f66e03d8a13fcc3c703f134950c0
Dustin Cote <dustincote@hotmail.com>
2020-11-23 20:30:13 -0600

    base tables set up

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import WorkOrdersTable from './WorkOrdersTable';
+
+//material ui imports
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 class UserPage extends Component {
   // this component doesn't do much to start, just renders some user info to the DOM
@@ -10,6 +15,17 @@ class UserPage extends Component {
       <div>
         <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
         <p>Your ID is: {this.props.store.user.id}</p>
+        <Grid container justify='center' alignItems='center'>
+          <Grid item style={{textAlign:'center'}} xs={12}>
+            <Button color='primary' variant='contained' style={{ margin: 30 }}>Create New Work Order</Button>
+          </Grid>
+          <Grid item xs={11} md={6}>
+            <WorkOrdersTable heading='Work Orders to Assign' />
+          </Grid>
+          <Grid item xs={11} md={6}>
+            <WorkOrdersTable heading='Current Work Orders' />
+          </Grid>
+        </Grid>
         <LogOutButton className="log-in" />
       </div>
     );
