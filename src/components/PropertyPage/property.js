418be0969d1d0e3cf09a8c9467e96f0c6d9642b0
Jeffrey L McMahon <jeffmcmahon1973@gmail.com>
2020-11-24 09:47:57 -0600

    first draft of property page complete

--- src/components/PropertyPage/property.js
+++ src/components/PropertyPage/property.js
@@ -1,33 +1,83 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import WorkOrderTableItem from './WorkOrderTableItem';
+
+//material-ui imports
+import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { withStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+    root: {
+        marginTop: 10,
+        marginBottom: 40,
+        maxHeight: 400
+    },
+    heading: {
+        padding: 15,
+        textAlign: 'center'
+    },
+    tableHeading: {
+        textAlign: 'center',
+    },
+    row: {
+        '&:hover': { cursor: 'pointer' }
+    },
+    newOrder: {
+        margin: 20,
+        backgroundColor: 'yellow',
+        '&:hover': { backgroundColor: 'green', color: 'white' }
+    },
+});
+
 
 class property extends Component {
 
     componentDidMount = () => {
-        this.props.dispatch({ type: "FETCH_PROPERTY"});
+        this.props.dispatch({ type: "FETCH_PROPERTY" });
     };
 
-  render() {
-    return (
-      <div>
-        <h1> Properties</h1>
-        <h2>Current Properties</h2>
-        <table>
-            <tr>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Details</th>
-            </tr>
-            <tr>
-                
-            </tr>
-        </table>
+    render() {
 
-      </div>
-    );
-  }
+        return (
+            <>
+                <Grid container justify='center'>
+
+                    <Grid item xs={11}>
+                        <Paper>
+                            <Typography variant='h3' className={classes.heading}>{this.props.heading}</Typography>
+                            <TableContainer className={classes.root} component={Paper}>
+                                <Table stickyHeader size='medium'>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell className={classes.tableHeading}>Name</TableCell>
+                                            <TableCell className={classes.tableHeading}>Address</TableCell>
+                                            <TableCell className={classes.tableHeading}>Details</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {this.props.properties.map((property) => {
+                                                <PropertyTableItem property={property} key={property.id} />
+                                            })}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </>
+        );
+    }
 }
 
 
-export default connect(mapStoreToProps)(property);
+export default connect()(withStyles(styles, { withTheme: true })(property));
+
