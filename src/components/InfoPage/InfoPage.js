7908e5ac037d4d6dbad284f21334343ca1e33939
Sarah Carlisle <sarah@travelescaperoom.com>
2020-11-29 20:11:29 -0600

    css on user profile

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -5,15 +5,23 @@ import { withRouter } from 'react-router-dom';
 //pages to bring in
 import InfoItem from './InfoItem';
 import PasswordItem from './PasswordItem';
+import './InfoPage.css'
 
 //material ui imports
-import Grid from '@material-ui/core/Grid';
-import InputLabel from '@material-ui/core/InputLabel';
-import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import { object, ref, string } from 'yup'
+
+const styles = theme => ({
+  center: {
+      [theme.breakpoints.down('sm')]: {
+          textAlign: 'center'
+      },
+      [theme.breakpoints.up('md')]: {
+          textAlign: 'right'
+      }
+  },
+})
+
 
 class UserProfile extends Component {
   state = {
@@ -38,15 +46,17 @@ class UserProfile extends Component {
     console.log(this.props)
     const user1=this.props.user
     return (
-      <div>
+      <div className="userProfile">
+        
         {!this.state.edit && <><h1>{this.props.user.first_name}'s Profile</h1>
         <h3>Username: {this.props.user.username}</h3>
         <h3>Name: {this.props.user.first_name} {this.props.user.last_name}</h3>
         <h3>Email: {this.props.user.email}</h3>
-        <button onClick={this.editUser}>Edit Profile</button></>}
+        <br></br>
+        <Button color="primary" variant="contained" onClick={this.editUser}>Edit Profile</Button></>}
         {this.state.edit && this.props.user.id && <InfoItem clearEdit = {this.clearEdit} user={user1} username={this.props.user.username} />}
-        
-        <button onClick={this.editUser}>Change Password</button>
+        <>  </>
+        <Button color="primary" variant="contained" onClick={this.editUser}>Change Password</Button>
         {this.state.edit && this.props.user.id && <PasswordItem clearEdit = {this.clearEdit} user={user1} username={this.props.user.username} />}
 
       </div>
@@ -56,4 +66,4 @@ class UserProfile extends Component {
 
 const mapStateToProps = (state) => ({ user: state.user })
 
-export default connect(mapStateToProps)(withRouter(UserProfile));
+export default connect(mapStateToProps)(withRouter(withStyles(styles, { withTheme: true })(UserProfile)));
--- src/components/InfoPage/PasswordItem.js
+++ src/components/InfoPage/PasswordItem.js
@@ -17,187 +17,184 @@ import Alert from './Alert'
 
 export default class EditUserPassword extends Component {
     state = {
-      password: false,
-    }
-  
-    _handleModalClose = () => {
-      this.setState(() => ({
         password: false,
-      }))
     }
-  
-    _renderModal = () => {
-      const onClick = () => {
-        this.setState(() => ({ password: false }))
-      }
-  
-      return (
-        <Alert
-          isOpen={this.state.password}
-          onClose={this._handleClose}
-          handleSubmit={onClick}
-          title="Password Reset"
-          text="Your password was changed successfully"
-          submitButtonText="Done"
-        />
-      )
-    }
-  
-    _handleSubmit = ({
-      oldPassword,
-      password,
-      confirmPassword,
-      setSubmitting,
-      resetForm,
-    }) => {
-      // fake async login
-      setTimeout(async () => {
-        setSubmitting(false)
-  
+
+    handleModalClose = () => {
         this.setState(() => ({
-          password: true,
+            password: false,
         }))
-  
-        resetForm()
-      }, 1000)
     }
-  
-    render() {
-      return (
-        <Formik
-          initialValues={{
+
+    renderModal = () => {
+        const onClick = () => {
+            this.setState(() => ({ password: false }))
+        }
+
+        return (
+            <Alert
+                isOpen={this.state.password}
+                onClose={this._handleClose}
+                handleSubmit={onClick}
+                title="Password Reset"
+                text="Your password was changed successfully"
+                submitButtonText="Done"
+            />
+        )
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.dispatch({
+            type: "UPDATE_PASSWORD",
+            payload: { ...this.state, id: this.props.user.id }
+        })
+        this.setState({
             oldPassword: '',
             password: '',
             confirmPassword: '',
-          }}
-          validationSchema={object().shape({
-            oldPassword: string().required('Current password is required'),
-            password: string().required('New password is required'),
-            confirmPassword: string()
-              .oneOf([ref('password')], 'Passwords do not match')
-              .required('Password is required'),
-          })}
-          onSubmit={(
-            { oldPassword, password, confirmPassword },
-            { setSubmitting, resetForm }
-          ) =>
-            this._handleSubmit({
-              oldPassword,
-              password,
-              confirmPassword,
-              setSubmitting,
-              resetForm,
-            })
-          }
-          render={props => {
-            const {
-              values,
-              touched,
-              errors,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isValid,
-              isSubmitting,
-            } = props
-            return isSubmitting ? (
-              <Spinner />
-            ) : (
-              <Paper>
-                <form className="form" onSubmit={handleSubmit}>
-                  <FormControl fullWidth margin="dense">
-                    <InputLabel
-                      htmlFor="password-current"
-                      error={Boolean(touched.oldPassword && errors.oldPassword)}
-                    >
-                      {'Current Password'}
-                    </InputLabel>
-                    <Input
-                      id="password-current"
-                      name="oldPassword"
-                      type="password"
-                      value={values.oldPassword}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={Boolean(touched.oldPassword && errors.currentPass)}
-                    />
-                    <FormHelperText
-                      error={Boolean(touched.currentPass && errors.currentPass)}
-                    >
-                      {touched.currentPass && errors.currentPass
-                        ? errors.currentPass
-                        : ''}
-                    </FormHelperText>
-                  </FormControl>
-                  <FormControl
-                    fullWidth
-                    margin="dense"
-                    error={Boolean(touched.password && errors.password)}
-                  >
-                    <InputLabel
-                      htmlFor="password-new"
-                      error={Boolean(touched.password && errors.password)}
-                    >
-                      {'New Password'}
-                    </InputLabel>
-                    <Input
-                      id="password-new"
-                      name="password"
-                      type="password"
-                      value={values.password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={Boolean(touched.password && errors.password)}
-                    />
-                    <FormHelperText
-                      error={Boolean(touched.password && errors.password)}
-                    >
-                      {touched.password && errors.password ? errors.password : ''}
-                    </FormHelperText>
-                  </FormControl>
-                  <FormControl
-                    fullWidth
-                    margin="dense"
-                    error={Boolean(touched.confirmPassword && errors.confirmPassword)}
-                  >
-                    <InputLabel
-                      htmlFor="password-confirm"
-                      error={Boolean(touched.confirmPassword && errors.confirmPassword)}
-                    >
-                      {'Confirm Password'}
-                    </InputLabel>
-                    <Input
-                      id="password-confirm"
-                      name="confirmPassword"
-                      type="password"
-                      value={values.confirmPassword}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={Boolean(touched.confirmPassword && errors.confirmPassword)}
-                    />
-                    <FormHelperText
-                      error={Boolean(touched.confirmPassword && errors.confirmPassword)}
-                    >
-                      {touched.confirmPassword && errors.confirmPassword
-                        ? errors.confirmPassword
-                        : ''}
-                    </FormHelperText>
-                  </FormControl>
-                  <Button
-                    type="submit"
-                    variant="raised"
-                    color="primary"
-                    disabled={Boolean(!isValid || isSubmitting)}
-                    style={{ margin: '16px' }}
-                  >
-                    {'Reset Password'}
-                  </Button>
-                </form>
-                {this._renderModal()}
-              </Paper>
-            )
-          }}
-        />
-      )
+            setSubmitting: '',
+            resetForm: '',
+        })
     }
-  }
+
+    render() {
+        return (
+            <Formik
+                initialValues={{
+                    oldPassword: '',
+                    password: '',
+                    confirmPassword: '',
+                }}
+                // validation help from 'yup'
+                validationSchema={object().shape({
+                    oldPassword: string().required('Current password is required'),
+                    password: string().required('New password is required'),
+                    confirmPassword: string()
+                        .oneOf([ref('password')], 'Passwords do not match')
+                        .required('Password is required'),
+                })}
+                onSubmit={(
+                    { oldPassword, password, confirmPassword },
+                    { setSubmitting, resetForm }
+                ) =>
+                    this.handleSubmit({
+                        oldPassword,
+                        password,
+                        confirmPassword,
+                        setSubmitting,
+                        resetForm,
+                    })
+                }
+                render={props => {
+                    const {
+                        values,
+                        touched,
+                        errors,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isValid,
+                        isSubmitting,
+                    } = props
+                    return isSubmitting ? (
+                        <Spinner />
+                    ) : (
+                            <Paper>
+                                <form className="passWORD" onSubmit={handleSubmit}>
+                                    <FormControl fullWidth margin="dense">
+                                        <InputLabel
+                                            htmlFor="password-current"
+                                            error={Boolean(touched.oldPassword && errors.oldPassword)}
+                                        >
+                                            {'Current Password'}
+                                        </InputLabel>
+                                        <Input
+                                            id="password-current"
+                                            name="oldPassword"
+                                            type="password"
+                                            value={values.oldPassword}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            error={Boolean(touched.oldPassword && errors.currentPass)}
+                                        />
+                                        <FormHelperText
+                                            error={Boolean(touched.currentPass && errors.currentPass)}
+                                        >
+                                            {touched.currentPass && errors.currentPass
+                                                ? errors.currentPass
+                                                : ''}
+                                        </FormHelperText>
+                                    </FormControl>
+                                    <FormControl
+                                        fullWidth
+                                        margin="dense"
+                                        error={Boolean(touched.password && errors.password)}
+                                    >
+                                        <InputLabel
+                                            htmlFor="password-new"
+                                            error={Boolean(touched.password && errors.password)}
+                                        >
+                                            {'New Password'}
+                                        </InputLabel>
+                                        <Input
+                                            id="password-new"
+                                            name="password"
+                                            type="password"
+                                            value={values.password}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            error={Boolean(touched.password && errors.password)}
+                                        />
+                                        <FormHelperText
+                                            error={Boolean(touched.password && errors.password)}
+                                        >
+                                            {touched.password && errors.password ? errors.password : ''}
+                                        </FormHelperText>
+                                    </FormControl>
+                                    <FormControl
+                                        fullWidth
+                                        margin="dense"
+                                        error={Boolean(touched.confirmPassword && errors.confirmPassword)}
+                                    >
+                                        <InputLabel
+                                            htmlFor="password-confirm"
+                                            error={Boolean(touched.confirmPassword && errors.confirmPassword)}
+                                        >
+                                            {'Confirm Password'}
+                                        </InputLabel>
+                                        <Input
+                                            id="password-confirm"
+                                            name="confirmPassword"
+                                            type="password"
+                                            value={values.confirmPassword}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            error={Boolean(touched.confirmPassword && errors.confirmPassword)}
+                                        />
+                                        <FormHelperText
+                                            error={Boolean(touched.confirmPassword && errors.confirmPassword)}
+                                        >
+                                            {touched.confirmPassword && errors.confirmPassword
+                                                ? errors.confirmPassword
+                                                : ''}
+                                        </FormHelperText>
+                                    </FormControl>
+                                    <Button
+                                        type="submit"
+                                        variant="raised"
+                                        color="primary"
+                                        disabled={Boolean(!isValid || isSubmitting)}
+                                        style={{ margin: '16px' }}
+                                    >
+                                        {'Reset Password'}
+                                    </Button>
+                                </form>
+                                {this.renderModal()}
+                            </Paper>
+                        )
+                }}
+            />
+        )
+    }
+}
