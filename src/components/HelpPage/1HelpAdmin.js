87e2294610bd3a2cd936351f3ba916297c4f7287
Sarah Carlisle <sarah@travelescaperoom.com>
2020-12-07 10:26:49 -0600

    RC instruction page complete

--- src/components/HelpPage/1HelpAdmin.js
+++ src/components/HelpPage/1HelpAdmin.js
@@ -5,8 +5,7 @@ import { withRouter } from 'react-router-dom';
 //material ui imports
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import { Typography } from '@material-ui/core';
-import { withStyles } from '@material-ui/core';
+import { Typography, withStyles } from '@material-ui/core';
 
 
 const styles = theme => ({
